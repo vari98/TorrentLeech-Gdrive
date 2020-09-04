b6d5afc921c6b703f5917de129387f5be7175635
A K A <66775180+vari98@users.noreply.github.com>
2020-09-04 12:04:11 +0530

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -130,7 +130,7 @@ class CloneHelper:
             button.append(
                 [
                     pyrogram.InlineKeyboardButton(
-                        text="☁️ CloudUrl ☁️",
+                        text="🌐 Cloud URL 🌐",
                         url=f"{gau_link}"
                     )
             ]
@@ -142,11 +142,11 @@ class CloneHelper:
                     indexurl = f"{INDEX_LINK}/{self.name}/"
                 tam_link = requests.utils.requote_uri(indexurl)
                 LOGGER.info(tam_link)
-                button.append([pyrogram.InlineKeyboardButton(text="ℹ️ IndexUrl ℹ️", url=f"{tam_link}")])
+                button.append([pyrogram.InlineKeyboardButton(text="🔗 Index URL 🔗", url=f"{tam_link}")])
             button_markup = pyrogram.InlineKeyboardMarkup(button)
             msg = await self.lsg.edit_text(
-                f"🤖: {_up} cloned successfully in your Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
-                \n📀 Info: Calculating...",
+                f"📂: <code>{_up}</code> <a href='tg://user?id={self.u_id}'>Cloned successfully in the Cloud 💨</a>\
+                \n💾 <b>Info :</b> <code>Calculating...</code>",
                 reply_markup=button_markup,
                 parse_mode="html"
             )
@@ -168,8 +168,8 @@ class CloneHelper:
             LOGGER.info(am.decode('utf-8'))
             await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
             await msg.edit_text(
-                f"🤖: {_up} cloned successfully in your Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
-                \n📀 Info:\n{g_autam}",
+                f"📂: <code>{_up}</code> <a href='tg://user?id={self.u_id}'>Cloned successfully in the Cloud 💨</a>\
+                \n💾 <b>Info :</b>\n<code>{g_autam}</code>",
                 reply_markup=button_markup,
                 parse_mode="html"
             )
@@ -177,7 +177,7 @@ class CloneHelper:
             
 		
     async def gcl(self):
-        self.lsg = await self.mess.reply_text(f"Cloning...you should wait 🤒")
+        self.lsg = await self.mess.reply_text(f"Cloning...you should wait 🤫")
         destination = f'{DESTINATION_FOLDER}'
         cmd = [
             "/app/gautam/gclone",
