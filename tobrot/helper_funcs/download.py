5a79fefc8d197537a4a4f2362ba3d3cc88f8ae35
A K A <66775180+vari98@users.noreply.github.com>
2020-09-04 12:08:06 +0530

    Update download.py

--- tobrot/helper_funcs/download.py
+++ tobrot/helper_funcs/download.py
@@ -44,7 +44,7 @@ async def down_load_media_f(client, message):
             file_name=download_location,
             progress=progress_for_pyrogram_g,
             progress_args=(
-                "trying to download", mess_age, c_time
+                "Trying to Download...", mess_age, c_time
             )
         )
         end_t = datetime.now()
